588a355891052e1c0c1162aaffc31656e693754c
Winster-Thuruthippillil JOSE <wjose@amadeus.com>
2016-09-27 12:11:09 +0530

    moving listening to bottom

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
-  console.log('App is running on port', app.get('port'));
-});
 exports = module.exports = app;
 
 require("./router");
 require("./websocket");
 require("./gcm");
+
+app.listen(app.get('port'), function() {
+  console.log('App is running on port', app.get('port'));
+});
